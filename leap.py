f23d2a61c47aaf10ea939b67fa70304b3d51b1d6
shebilantony <42166928+shebilantony@users.noreply.github.com>
2018-08-09 13:50:01 +0530

    Create leap.py

---
+++ leap.py
@@ -0,0 +1,11 @@
+years = 2000
+if (years % 4) == 0:
+   if (years % 100) == 0:
+       if (years % 400) == 0:
+           print("{0} is a leap years".format(years))
+       else:
+           print("{0} is not a leap years".format(years))
+   else:
+       print("{0} is a leap years".format(years))
+else:
+   print("{0} is not a leap years".format(years))
